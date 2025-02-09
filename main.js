9cba6249f2a00b30f528064858c5d561d89f082b
Daniel-Oshione <omobsdaniel@gmail.com>
2025-02-09 17:03:29 +0100

    finishing up sunnyside

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
 const menuContents = document.getElementById("menuContents");
 const togglebtn = document.getElementById("toggle");
+const closebtn = document.getElementById("close_menu");
 
 togglebtn.addEventListener("click", ()=>{
-    if (menuContents.style.display = "none"){
-        menuContents.style.display = "block";
-    }
-    else{
-        menuContents.style.display = "none";
-    }
+    menuContents.style.display = "block";
+    togglebtn.style.display = "none";
+    closebtn.style.display = "block";
+    // if (menuContents.style.display = "none"){
+    //     menuContents.style.display = "block";
+    // }
+    // else{
+    //     menuContents.style.display = "none";
+    // }
+})
+
+closebtn.addEventListener("click", ()=>{
+    menuContents.style.display = "none";
+    togglebtn.style.display = "block";
+    closebtn.style.display = "none";
 })
 
